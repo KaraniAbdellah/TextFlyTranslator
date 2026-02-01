1474ebb9421471174b6a1849af6ca0fb59f4b2b2
KaraniAbdellah <abdellahkarani@gmail.com>
2026-02-01 16:35:07 +0100

    feat: update method to POST

---
+++ clients/phpClient/client.php
@@ -0,0 +1,20 @@
+<?php
+
+$url = "http://localhost:8080/demo-1.0-SNAPSHOT/translator/getTranslation";
+
+$data = [
+    "prompt" => "Translate this <Hello World> to this language <Arabic>"
+];
+
+$options = [
+    "http" => [
+        "method"  => "POST",
+        "header"  => "Content-Type: application/json\r\n",
+        "content" => json_encode($data)
+    ]
+];
+
+$context = stream_context_create($options);
+$response = file_get_contents($url, false, $context);
+
+echo $response;
--- demo/src/main/java/com/example/demo/TranslatorOperations.java
+++ demo/src/main/java/com/example/demo/TranslatorOperations.java
@@ -1,34 +1,34 @@
 package com.example.demo;
 
-import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 
 import com.google.genai.Client;
 import com.google.genai.types.GenerateContentResponse;
 
 @Path("/getTranslation")
 public class TranslatorOperations {
-    // REST endpoint
-    @GET
-    @Produces("text/plain")
-    public String getTranslation() {
-        String apiKey = System.getenv("GOOGLE_API_KEY");
-        System.out.println(apiKey);
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTranslation(PromptRequest req) {
+        System.out.println("Hello from Get Translation");
         Client client = new Client();
         GenerateContentResponse response =
                 client.models.generateContent(
                         "gemini-3-flash-preview",
-                        "Translate this <Hello World> to this langauge <Arabic>",
-                        null);
-        System.out.println(response.text());
+                        req.prompt,
+                        null
+                );
+
         return response.text();
     }
+
+    public class PromptRequest {
+        public String prompt;
+    }
 }
-
-// NOTE: each time you must open wildFly server --> /bin/standlone.sh --> and before i need to setUp api key
-// export GOOGLE_API_KEY=API_KEY
-// /home/abdellah/wildfly-39.0.0.Beta1/bin/standalone.sh
-
-
